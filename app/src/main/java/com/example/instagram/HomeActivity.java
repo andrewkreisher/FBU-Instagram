3c6d7f58197e399a1dfa36d19f94457d5dca2bfb
andrewkreisher <andrewkreisher@fb.com>
2019-07-08 17:04:39 -0700

    Add logout functionality

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -1,21 +1,38 @@
 package com.example.instagram;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.instagram.model.Post;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button logoutBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logoutBtn = findViewById(R.id.btnLogout);
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Intent logout2login = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(logout2login);
+            }
+        });
 
         final Post.Query postsQuery = new Post.Query();
         postsQuery.getTop().withUser();
